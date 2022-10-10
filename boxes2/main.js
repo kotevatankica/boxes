6dae679fc68807eb6ffa9c3ebd6cd5ef90a5dc92
kotevatankica <koteva.tankica@gmail.com>
2022-10-10 12:58:33 +0200

    something:

--- boxes2/main.js
+++ boxes2/main.js
@@ -1,25 +1,24 @@
-import './style.css'
+import "./style.css";
 let $grid = document.getElementById("wrapper");
 let row = +document.querySelector('[name="rows"]').value;
 let tile = +document.querySelector('[name="columns"]').value;
 let $tiles = document.querySelectorAll(".tile");
 let $form_selector = document.querySelector('[name="tile-form"]');
-
+let interval;
 let disableClick = false;
 
-const drawGrid = ($grid,row,tile, tile_form='') => {
-
-  let local_tile_form = ''
+const drawGrid = ($grid, row, tile, tile_form = "") => {
+  let local_tile_form = "";
 
   switch (tile_form) {
-    case 'circle':
-      local_tile_form = tile_form
+    case "circle":
+      local_tile_form = tile_form;
       break;
-    case 'rounded':
-      local_tile_form = tile_form
+    case "rounded":
+      local_tile_form = tile_form;
       break;
     default:
-      local_tile_form = ''
+      local_tile_form = "";
       break;
   }
 
@@ -36,118 +35,173 @@ const drawGrid = ($grid,row,tile, tile_form='') => {
   $grid.innerHTML = final_string;
 
   $tiles = document.querySelectorAll(".tile");
-}
-drawGrid($grid,row,tile,document.querySelector('[name="tile-form"]').value)
-
+};
+drawGrid($grid, row, tile, document.querySelector('[name="tile-form"]').value);
 
 const selectedTiles = [];
 
 $grid.addEventListener("click", (event) => {
-  
-  if(disableClick)
-    return;
+  if (disableClick) return;
   if (event.target.classList.contains("tile"))
     event.target.classList.add("selected");
   else console.log("it's not a tile");
 
-    if(event.target.classList.contains("selected"))
+  if (event.target.classList.contains("selected"))
     selectedTiles.push(event.target);
 
+  let form_direction = document.querySelector('[name="direction"]').value;
 
-    let form_direction = document.querySelector('[name="direction"]').value;
+  let delay = 500;
 
-      
-    let delay = 500;
+  let new_array = Array.from(selectedTiles);
 
-
-    let new_array = Array.from(selectedTiles);
-  
-    if(selectedTiles.length === $tiles.length){
-      disableClick=true;
-      if(form_direction === 'filo'){
-    
-        new_array.reverse();
-  
-
-      }
-
-          
-      for (let index = 0; index < new_array.length; index++) {
-        const tile = new_array[index];
-        delay = delay + 500;
-        setTimeout(()=>{
-          if(form_direction =='filo')
-            selectedTiles.shift()
-          else
-            selectedTiles.pop()
-
-          if(selectedTiles.length === 0){
-            disableClick = false
-          }
-          tile.classList.remove('selected');
-        },delay)
-      }
+  if (selectedTiles.length === $tiles.length) {
+    disableClick = true;
+    if (form_direction === "filo") {
+      new_array.reverse();
     }
+
+    for (let index = 0; index < new_array.length; index++) {
+      const tile = new_array[index];
+      delay = delay + 500;
+      setTimeout(() => {
+        if (form_direction == "filo") selectedTiles.shift();
+        else selectedTiles.pop();
+
+        if (selectedTiles.length === 0) {
+          disableClick = false;
+        }
+        tile.classList.remove("selected");
+      }, delay);
+    }
+  }
 });
 
-
-
-
-
-$form_selector.addEventListener('change', (event)=>{
+$form_selector.addEventListener("change", (event) => {
   let form_type = event.target.value;
   changeTileForm(form_type);
 });
 
-document.querySelector('#rows').addEventListener('change',(event)=>{
-  happensOnDimesionChange()
-})
+document.querySelector("#rows").addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) happensOnDimesionChange();
+});
 
-document.querySelector('#columns').addEventListener('change',(event)=>{
-  happensOnDimesionChange()
-})
+document.querySelector("#columns").addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) happensOnDimesionChange();
+});
 
-const happensOnDimesionChange = () =>{
-  let rows = +document.querySelector('#rows').value;
-  let columns = +document.querySelector('#columns').value;
-  rows = (rows>0) ? rows : 1;
-  columns = (columns>0) ? columns :1
-  rows = Math.min(rows,20);
-  columns =Math.min(columns,8);
-  document.querySelector('#rows').value = rows;
-  document.querySelector('#columns').value = columns;
-  drawGrid($grid,rows,columns,document.querySelector('[name="tile-form"]').value)
+const happensOnDimesionChange = () => {
+  let rows = +document.querySelector("#rows").value;
+  let columns = +document.querySelector("#columns").value;
+  rows = rows > 0 ? rows : 1;
+  columns = columns > 0 ? columns : 1;
+  rows = Math.min(rows, 20);
+  columns = Math.min(columns, 8);
+  document.querySelector("#rows").value = rows;
+  document.querySelector("#columns").value = columns;
+  drawGrid(
+    $grid,
+    rows,
+    columns,
+    document.querySelector('[name="tile-form"]').value
+  );
 
   //TODO: message from lazayvlad -> after you draw the grid you need to clean the selectedTiles
-}
+};
+const unload = (selectedTiles, direction, delay) => {
+  if (direction === "fifo") {
+    if (interval && selectedTiles.length === 0) {
+      clearInterval(interval);
+    }
+    interval = setInterval(() => {
+      const element = selectedTiles.shift();
 
+      if (element) {
+        element.classList.remove("selected");
+      } else {
+        disableClick = false;
+        return;
+      }
 
+      if (selectedTiles.length === 0) {
+        clearInterval(interval);
+        disableClick = false;
+      }
+    }, delay);
+  }
 
-const changeTileForm = (form_type = 'circle') => {
-  console.log(`%c u boja`,'background:black;color:red')
-  if(form_type === 'circle'){
-    Array.from($tiles).forEach((tile)=>{
-      tile.classList.add('circle');
-      tile.classList.remove('rounded');
+  if (direction === "filo") {
+    if (interval && selectedTiles.length === 0) {
+      clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+      const element = selectedTiles.pop();
+      if (element) {
+        element.classList.remove("selected");
+      } else {
+        disableClick = false;
+        return;
+      }
+
+      if (selectedTiles.length === 0) {
+        clearInterval(interval);
+        disableClick = false;
+      }
+    }, delay);
+  }
+};
+
+const unloadWrap = () => {
+  disableClick = true;
+  let direction = document.querySelector('[name="direction"]').value;
+  let delay = document.querySelector('[name="delay"]').value;
+  unload(selectedTiles, direction, delay);
+};
+
+document.getElementById("unload").addEventListener("click", (event) => {
+  if (interval && selectedTiles.length > 0) {
+    if (selectedTiles.length === 0) {
+      clearInterval(interval);
+    }
+
+    if (!disableClick) {
+      clearInterval(interval);
+      unloadWrap();
+    }
+    return;
+  }
+  if (!disableClick) {
+    unloadWrap();
+  }
+});
+
+let draw = document.getElementById("draw");
+draw.addEventListener("click", (event) => {
+  happensOnDimesionChange();
+});
+
+const changeTileForm = (form_type = "circle") => {
+  // console.log(`%c u boja`, "background:black;color:red");
+  if (form_type === "circle") {
+    Array.from($tiles).forEach((tile) => {
+      tile.classList.add("circle");
+      tile.classList.remove("rounded");
     });
-   }
-   else if(form_type === 'square'){
-    Array.from($tiles).forEach((tile)=>{
-       tile.classList.remove('circle');
-       tile.classList.remove('rounded');
-     });
-   }
-   else if(form_type === 'rounded'){
-    Array.from($tiles).forEach((tile)=>{
-       tile.classList.add('rounded');
-       tile.classList.remove('circle');
-     });
-   }
-   else{ 
-      Array.from($tiles).forEach((tile)=>{
-        tile.classList.remove('rounded');
-      });
-   }
-}
+  } else if (form_type === "square") {
+    Array.from($tiles).forEach((tile) => {
+      tile.classList.remove("circle");
+      tile.classList.remove("rounded");
+    });
+  } else if (form_type === "rounded") {
+    Array.from($tiles).forEach((tile) => {
+      tile.classList.add("rounded");
+      tile.classList.remove("circle");
+    });
+  } else {
+    Array.from($tiles).forEach((tile) => {
+      tile.classList.remove("rounded");
+    });
+  }
+};
 changeTileForm($form_selector.value);
-
